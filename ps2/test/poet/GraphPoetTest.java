b9c251974aa89da015ff82c6b687b8ba9d36515f
AhsanSajjad322 <ahsansajjad57135@gmail.com>
2023-11-24 15:26:53 +0500

    Test cases GraphPoet

--- ps2/test/poet/GraphPoetTest.java
+++ ps2/test/poet/GraphPoetTest.java
@@ -5,6 +5,9 @@ package poet;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 
 /**
@@ -13,13 +16,49 @@ import org.junit.Test;
 public class GraphPoetTest {
     
     // Testing strategy
-    //   TODO
+
+    // Testing strategy for poem():
+    //      num bridge words - 0, 1, n
+    //      multiple bridges weight - same, different
+    //      multiple lines
+
+    
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
-    // TODO tests
+    // tests
+    
+    // covers num bridge - 0
+    @Test
+    public void testNoBridge() throws IOException {
+        GraphPoet poet = new GraphPoet(new File("test/poet/Where-no-man-has-gone-before.txt"));
+        String poem = poet.poem("no bridge");
+        
+        assertEquals("expected no bridge words",poem, "no bridge");
+    }
+    
+    // covers num bridge - 1
+    @Test
+    public void testOneBridge() throws IOException {
+        GraphPoet poet = new GraphPoet(new File("test/poet/one-bridge.txt"));
+        String poem = poet.poem("one bridge");
+        
+        assertEquals("expected one bridge word",poem, "one simple bridge");
+    }
+    
+    // covers num bridge - n
+    //        bridge - same weight
+    //        case insensitive
+    @Test
+    public void testMultipleBridgesSameWeightCaseInsensitive() throws IOException {
+        GraphPoet poet = new GraphPoet(new File("test/poet/multiple-bridges-same-weight-case-insensitive.txt"));
+        String poem = poet.poem("MULTIPLE bridges");
+        
+        assertTrue("expected multiple bridge words with same weight", 
+                   poem.equals("MULTIPLE same bridges") || poem.equals("MULTIPLE weight bridges"));
+    }
     
 }
